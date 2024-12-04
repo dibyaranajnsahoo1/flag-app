17ce63f14dda5cd4e2a9915f48feb938a96a9733
Dibya Ranjan sahoo <ranjandibya34512@gmail.com>
2024-12-04 14:14:43 +0530

    add

--- src/components/flag.jsx
+++ src/components/flag.jsx
@@ -2,26 +2,16 @@ import React, { useState, useEffect } from "react";
 import FlagIteams from './Flagitems';
 
 const Flag = () => {
+    const url = 'https://xcountries-backend.azurewebsites.net/all';
     const [countries, setCountries] = useState([]);
 
     useEffect(() => {
-        fetchUrl();
-    }, []);
-
-    const fetchUrl = () => {
-        const url = 'https://xcountries-backend.azurewebsites.net/all';
-
         fetch(url)
-            .then((res) => {
-                if (!res.ok) {
-                    
-                    throw new Error(`HTTP error! Status: ${res.status}`);
-                }
-                return res.json();
-            })
-            .then((data) => {setCountries(data);})
-            .catch((error) => { console.error("Failed to fetch countries data:", error.message); });
-    };
+            .then((res) => res.json())
+            .then((data) => setCountries(data))
+            .catch((error) => console.error("Failed to fetch countries data:", error.message))
+        
+    }, []);
 
     return (
         <div style={{ display: "flex", flexWrap: 'wrap', gap: '10px', alignItems: 'center', justifyContent: 'center' }}>
